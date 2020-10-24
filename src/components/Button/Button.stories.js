9ad0948bc46c7a49c60cfdb6c1ec2245fc9504a2
jrmoore117 <jrmoore117@gmail.com>
2020-10-24 15:00:36 -0700

    Add stories for every variant of primary and secondary buttons.

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -8,7 +8,83 @@ export default {
 
 const Template = args => <Button {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-   label: 'Save changes'
+export const Primary = Template.bind({});
+Primary.args = {
+   label: 'Save changes',
+};
+
+export const PrimaryBlue = Template.bind({});
+PrimaryBlue.args = {
+   label: 'Save changes',
+   variant: 'primary',
+   color: 'blue',
+};
+
+export const PrimaryRed = Template.bind({});
+PrimaryRed.args = {
+   label: 'Save changes',
+   variant: 'primary',
+   color: 'red',
+};
+
+export const PrimaryGreen = Template.bind({});
+PrimaryGreen.args = {
+   label: 'Save changes',
+   variant: 'primary',
+   color: 'green',
+};
+
+export const PrimaryPurple = Template.bind({});
+PrimaryPurple.args = {
+   label: 'Save changes',
+   variant: 'primary',
+   color: 'purple',
+};
+
+export const PrimaryIndigo = Template.bind({});
+PrimaryIndigo.args = {
+   label: 'Save changes',
+   variant: 'primary',
+   color: 'indigo',
+};
+
+export const SecondaryDefault = Template.bind({});
+SecondaryDefault.args = {
+   label: 'Save changes',
+   variant: 'secondary',
+};
+
+export const SecondaryBlue = Template.bind({});
+SecondaryBlue.args = {
+   label: 'Save changes',
+   variant: 'secondary',
+   color: 'blue',
+};
+
+export const SecondaryRed = Template.bind({});
+SecondaryRed.args = {
+   label: 'Save changes',
+   variant: 'secondary',
+   color: 'red',
+};
+
+export const SecondaryGreen = Template.bind({});
+SecondaryGreen.args = {
+   label: 'Save changes',
+   variant: 'secondary',
+   color: 'green',
+};
+
+export const SecondaryPurple = Template.bind({});
+SecondaryPurple.args = {
+   label: 'Save changes',
+   variant: 'secondary',
+   color: 'purple',
+};
+
+export const SecondaryIndigo = Template.bind({});
+SecondaryIndigo.args = {
+   label: 'Save changes',
+   variant: 'secondary',
+   color: 'indigo',
 };
