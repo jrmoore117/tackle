c9047755ac619f5f24f5574f9f7a41f382ae89e0
jrmoore117 <jrmoore117@gmail.com>
2020-10-24 19:55:19 -0700

    Add as, type, isDisabled, isSmall, isLoading, className, leftIcon, and rightIcon props as well as corresponding component logic.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 import { Button as Btn } from "reakit/Button";
+import { Loader } from 'react-feather';
 import PropTypes from 'prop-types';
 
-const Button = ({ variant, color, label, ...props }) => {
+const Button = ({
+   as,
+   variant,
+   color,
+   label,
+   isDisabled,
+   isSmall,
+   isLoading,
+   className,
+   leftIcon,
+   rightIcon,
+   ...props
+}) => {
    return (
       <Btn
-         className={`button button--${variant}--${color}`}
+         as={as}
+         className={`
+            button button--${variant}--${color}
+            ${isDisabled && `button--${variant}--disabled`}
+            ${isSmall && 'button--primary--small'}
+            ${className || null}
+         `}
          onAnimationEnd={(e) => e.target.blur()}
+         disabled={isLoading || isDisabled}
          {...props}
       >
-         {label}
+         {leftIcon && !isLoading ? cloneElement(leftIcon, { className: "h-4 w-4 mr-2" }) : null}
+         {isLoading ? <Loader className="mx-2 text-gray-700 animate-spin" /> : label}
+         {rightIcon && !isLoading ? cloneElement(rightIcon, { className: "h-4 w-4 ml-2" }) : null}
       </Btn>
    );
 }
 
 Button.defaultProps = {
+   as: 'button',
+   type: 'button',
    variant: 'primary',
    color: 'default',
+   isDisabled: false,
+   isSmall: false,
+   isLoading: false,
 }
 
 Button.propTypes = {
+   as: PropTypes.string,
+   type: PropTypes.oneOf(['button', 'submit', 'reset']),
    variant: PropTypes.string,
    color: PropTypes.string,
-   label: PropTypes.string.isRequired,
+   label: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object,
+   ]).isRequired,
+   isDisabled: PropTypes.bool,
+   isSmall: PropTypes.bool,
+   isLoading: PropTypes.bool,
+   className: PropTypes.string,
+   leftIcon: PropTypes.object,
+   rightIcon: PropTypes.object,
 }
 
 export default Button;
