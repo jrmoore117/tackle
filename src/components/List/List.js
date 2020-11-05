025bed1086bf0cd31f447ae21608ce1035aa001c
jrmoore117 <jrmoore117@gmail.com>
2020-11-04 23:29:37 -0800

    Create List component - takes array of objects.

---
+++ src/components/List/List.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const List = ({
+   items,
+   color,
+   onClick,
+   className,
+   ...props
+}) => (
+   <ul
+      className={className}
+      {...props}
+   >
+      {items.map((item, i) => (
+         <li
+            className={`
+               ${i !== items.length - 1
+                  ? 'listitem'
+                  : 'listitem--last'}
+               ${color && `hover:text-${color}-500`}
+            `}
+            onClick={() => onClick(item, i)}
+            key={`item-${i}`} // todo: generate better unique keys
+         >
+            {item.label}
+         </li>
+      ))}
+   </ul>
+);
+
+List.defaultProps = {
+   variant: 'default',
+   color: 'blue',
+}
+
+List.propTypes = {
+   variant: PropTypes.string,
+   label: PropTypes.string,
+}
+
+export default List;
