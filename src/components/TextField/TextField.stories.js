f8b922864c749125641f4004ae8bca30c21f5107
jrmoore117 <jrmoore117@gmail.com>
2020-11-08 22:28:59 -0800

    Add example for work in progress components.

--- src/components/TextField/TextField.stories.js
+++ src/components/TextField/TextField.stories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TextField from './TextField';
-import FormMessage from './FormMessage';
+import FieldMessage from './FieldMessage';
+import Field from './Field';
 import Button from '../Button/Button';
 import Checkbox from "../Checkbox/Checkbox";
 import { Radio, RadioGroup } from '../Radio/Radio';
 import { useRadioState } from "reakit/Radio";
+import { Search, X } from 'react-feather';
 
 import useForm from '../../hooks/useForm';
 
@@ -49,19 +51,46 @@ export const FormExampleWithValidations = () => {
          <div className="flex mt-2">
             <div className="mr-2 flex-1">
                <TextField placeholder="First Name" {...set("firstName")} className="w-full" />
-               <FormMessage {...set("firstName")} />
+               <FieldMessage {...set("firstName")} />
             </div>
             <div className="flex-1">
                <TextField placeholder="Last Name" {...set("lastName")} className="w-full" />
-               <FormMessage {...set("lastName")} />
+               <FieldMessage {...set("lastName")} />
             </div>
          </div>
          <TextField placeholder="Username" {...set("username")} className="w-full mt-2" />
-         <FormMessage {...set("username")} />
+         <FieldMessage {...set("username")} />
          <TextField type="password" placeholder="Password" {...set("password")} className="w-full mt-2" />
-         <FormMessage {...set("password")} />
+         <FieldMessage {...set("password")} />
          <Checkbox {...set("savePassword")} label="Save Password?" className="mt-2 mr-2" />
          <Button type="submit" label="Submit" color="blue" className="mt-2" />
       </form>
    );
 }
+
+// export const FieldExample = () => {
+//    const { set, reset, handleSubmit, helpers } = useForm({
+//       initialValues: {
+//          firstName: '',
+//       },
+//       validators: {
+//          firstName: (value) => value.length < 5
+//             ? 'Your name must be at least 5 characters long. Sorry, Chad!'
+//             : null,
+//       },
+//    });
+//    const callback = (values) => {
+//       console.log("Callback working!", { ...values });
+//       reset();
+//    }
+//    return (
+//       <form onSubmit={(event) => handleSubmit(event, callback)} className="flex flex-col">
+//          <Field {...set("firstName")}>
+//             <Search className="h-5 w-5 stroke-2 text-gray-900 mr-2" />
+//             <TextField {...set("firstName")} />
+//             <X className="h-5 w-5 stroke-2 text-gray-900 ml-2" />
+//          </Field>
+//          <Button type="submit" label="Submit" color="blue" className="mt-2" />
+//       </form>
+//    );
+// }
