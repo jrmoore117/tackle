f0b207f9e4d5424f016074c47c878c73a3648acf
jrmoore117 <jrmoore117@gmail.com>
2020-11-09 22:10:35 -0800

    Add Field component - wraps and combines TextField with left and right Icons.

---
+++ src/components/TextField/Field.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const Field = ({
+   errors,
+   children,
+   ...props
+}) => {
+
+   const [focused, setFocused] = useState(false);   
+   const toggleFocus = (e) => {
+      e.type === 'focus'
+         ? setFocused(true)
+         : setFocused(false)
+   }
+
+   const childrenWithProps = children.map((child, i) => {
+
+      if (React.isValidElement(child) && child.type.id === 'TextField') {
+         return React.cloneElement(child, {
+            key: `textfield-${i}`,
+            onBlur: toggleFocus,
+            onFocus: toggleFocus,
+            // Used in TextField.js
+            withElements: true,
+         });
+      }
+
+      if (React.isValidElement(child) && child.type.id === 'FieldElementLeft') {
+         return React.cloneElement(child, {
+            key: `field-element-left-${i}`,
+            className: '',
+            ...child.props,
+         });
+      }
+
+      if (React.isValidElement(child) && child.type.id === 'FieldElementRight') {
+         return React.cloneElement(child, {
+            key: `field-element-right-${i}`,
+            ...child.props,
+         });
+      }
+
+      return child;
+
+   });
+   return (
+      <div
+         className={`
+            field
+            ${focused && !errors && 'field--focused'}
+            ${focused && errors && 'field--focused--error'}
+         `}
+         {...props}
+      >
+         {childrenWithProps}
+      </div>
+   );
+}
+
+export default Field;
