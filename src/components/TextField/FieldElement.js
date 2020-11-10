c2a2e17f8e358953deb6247b06f17c9e6fcb8c3d
jrmoore117 <jrmoore117@gmail.com>
2020-11-09 22:25:25 -0800

    Remove span wrapping icons.

--- src/components/TextField/FieldElement.js
+++ src/components/TextField/FieldElement.js
@@ -1,24 +1,14 @@
 import React, { cloneElement } from 'react';
 
-export const FieldElementLeft = ({ children, ...props }) => {
-   const icon = cloneElement(children, { className: 'h-5 w-5 stroke-2 text-gray-900 mr-2' })
-   return (
-      <span {...props}>
-         {icon}
-      </span>
-   );
-}
+export const FieldElementLeft = ({ children, ...props }) => (
+   cloneElement(children, { className: 'h-5 w-5 stroke-2 text-gray-900 mr-2' })
+);
 
 FieldElementLeft.id = 'FieldElementLeft';
 
-export const FieldElementRight = ({ children, ...props }) => {
-   const icon = cloneElement(children, { className: 'h-5 w-5 stroke-2 text-gray-900 ml-2' })
-   return (
-      <span {...props}>
-         {icon}
-      </span>
-   );
-}
+export const FieldElementRight = ({ children, ...props }) => (
+   cloneElement(children, { className: 'h-5 w-5 stroke-2 text-gray-900 ml-2' })
+);
 
 FieldElementRight.id = 'FieldElementRight';
 
