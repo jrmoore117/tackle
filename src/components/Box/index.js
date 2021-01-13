60c1f4237b8191fb065b03786327b065cec524a7
jrmoore117 <jrmoore117@gmail.com>
2021-01-12 22:57:07 -0800

    Change className short circuit to ternary, matching other components.

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -11,7 +11,7 @@ const Box = ({
    ...props
 }) => (
    <div
-      className={`box box--${variant}--${color} ${isRounded ? 'rounded-full' : 'rounded'} ${pingColor ? `animate-ping-${pingColor}` : ''} ${className || ''}`}
+      className={`box box--${variant}--${color} ${isRounded ? 'rounded-full' : 'rounded'} ${pingColor ? `animate-ping-${pingColor}` : ''} ${className ? className : ''}`}
       {...props}
    >
       {children}
