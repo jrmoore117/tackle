79b958d75737f4ed6b8d957deba322a95e94c9ef
jrmoore117 <jrmoore117@gmail.com>
2021-01-28 21:18:07 -0800

    Add tip to select light theme if dark theme is default.

--- src/components/Accordion/Accordion.stories.js
+++ src/components/Accordion/Accordion.stories.js
@@ -3,6 +3,7 @@ import { Accordion, Panel } from 'components/Accordion';
 import Box from 'components/Box';
 import useForm from 'hooks/useForm';
 import Switch from 'components/Switch';
+import Icon from 'components/Icon';
 
 export default {
    component: Accordion,
@@ -17,23 +18,26 @@ export const DefaultAccordion = () => {
       },
    })
    return (
-      <Accordion>
-         <Panel label="Profile">
-            <Box variant="frame" className="p-4">
-               Profile Info
-            </Box>
-         </Panel>
-         <Panel label="Notifications">
-            <Box variant="frame" className="p-4">
-               <Switch {...set("notifications")} label={`Notifications ${notifications ? 'on' : 'off'}`} />
-            </Box>
-         </Panel>
-         <Panel label="Security">
-            <Box variant="frame" className="p-4">
-               <Switch {...set("security")} label={`2FA ${security ? 'enabled' : 'disabled'}`} />
-            </Box>
-         </Panel>
-      </Accordion>
+      <div>
+         <p className="mb-10 flex items-center">If you default to dark theme, click <Icon as="Image" className="mx-2" /> above to select light theme.</p>
+         <Accordion>
+            <Panel label="Profile">
+               <Box variant="frame" className="p-4">
+                  Profile Info
+               </Box>
+            </Panel>
+            <Panel label="Notifications">
+               <Box variant="frame" className="p-4">
+                  <Switch {...set("notifications")} label={`Notifications ${notifications ? 'on' : 'off'}`} />
+               </Box>
+            </Panel>
+            <Panel label="Security">
+               <Box variant="frame" className="p-4">
+                  <Switch {...set("security")} label={`2FA ${security ? 'enabled' : 'disabled'}`} />
+               </Box>
+            </Panel>
+         </Accordion>
+      </div>
    );
 }
 
