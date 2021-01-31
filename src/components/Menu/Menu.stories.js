81016637d575832ebf6e377297957c9df45a88f2
jrmoore117 <jrmoore117@gmail.com>
2021-01-30 16:38:22 -0800

    Add Menu stories using new Menu props.

--- src/components/Menu/Menu.stories.js
+++ src/components/Menu/Menu.stories.js
@@ -10,7 +10,12 @@ export default {
 export const MenuWithIcon = () => {
    const [isOpen, setIsOpen] = React.useState(false);
    return (
-      <Menu icon="MoreHorizontal" isOpen={isOpen} toggle={setIsOpen}>
+      <Menu
+         isOpen={isOpen}
+         toggle={setIsOpen}
+         icon="MoreHorizontal"
+         className="mb-52"
+      >
          <MenuItem label="Hook" />
          <MenuItem label="Line" />
          <MenuItem label="Sinker" />
@@ -21,7 +26,7 @@ export const MenuWithIcon = () => {
    );
 }
 
-export const MenuWithLabel = () => {
+export const BlueMenuWithLabelAndIcons = () => {
    const [isOpen, setIsOpen] = React.useState(false);
    return (
       <Menu
@@ -29,10 +34,71 @@ export const MenuWithLabel = () => {
          toggle={setIsOpen}
          color="blue"
          label="Tackle"
+         className="mb-32"
+      >
+         <MenuItem icon="Share" label="Share" />
+         <MenuItem icon="Clipboard" label="Copy link" />
+         <MenuItem icon="Flag" label="Report" />
+      </Menu>
+   );
+}
+
+export const BottomRightMenu = () => {
+   const [isOpen, setIsOpen] = React.useState(false);
+   return (
+      <Menu
+         isOpen={isOpen}
+         toggle={setIsOpen}
+         icon="MoreHorizontal"
+         position="br"
+         className="mb-52"
       >
          <MenuItem label="Hook" />
          <MenuItem label="Line" />
          <MenuItem label="Sinker" />
+         <Divider className="my-2" />
+         <MenuItem label="Bobber" />
+         <MenuItem label="Lure" />
+      </Menu>
+   );
+}
+
+export const TopRightMenu = () => {
+   const [isOpen, setIsOpen] = React.useState(false);
+   return (
+      <Menu
+         isOpen={isOpen}
+         toggle={setIsOpen}
+         position="tr"
+         className="mt-52"
+         icon="MoreHorizontal"
+      >
+         <MenuItem label="Hook" />
+         <MenuItem label="Line" />
+         <MenuItem label="Sinker" />
+         <Divider className="my-2" />
+         <MenuItem label="Bobber" />
+         <MenuItem label="Lure" />
+      </Menu>
+   );
+}
+
+export const TopLeftMenu = () => {
+   const [isOpen, setIsOpen] = React.useState(false);
+   return (
+      <Menu
+         isOpen={isOpen}
+         toggle={setIsOpen}
+         position="tl"
+         className="mt-52"
+         icon="MoreHorizontal"
+      >
+         <MenuItem label="Hook" />
+         <MenuItem label="Line" />
+         <MenuItem label="Sinker" />
+         <Divider className="my-2" />
+         <MenuItem label="Bobber" />
+         <MenuItem label="Lure" />
       </Menu>
    );
 }
