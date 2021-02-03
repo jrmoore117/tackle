16a8996a06b1c7dfa174516a7c5d56a8fefe3ae8
jrmoore117 <jrmoore117@gmail.com>
2021-02-02 18:26:22 -0800

    Add color prop to Tooltip component. Add purple Tooltip story.

--- src/components/Tooltip/Tooltip.stories.js
+++ src/components/Tooltip/Tooltip.stories.js
@@ -7,24 +7,29 @@ export default {
    title: 'Tooltip',
 };
 
-export const TooltipWithLabelAndIcons = () => {
-   const [isOpen, setIsOpen] = React.useState(false);
-   return (
-      <div className="my-20">
-         <Tooltip
-            content={
-               <div>
-                  <div className="font-semibold">
-                     Tooltip
-                  </div>
-                  <div className="text-sm">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </div>
+export const TooltipWithContent = () => (
+   <div className="my-20">
+      <Tooltip
+         content={
+            <div>
+               <div className="font-semibold">
+                  Tooltip
                </div>
-            }
-         >
-            <Icon as="Info" color="blue" variant="shaded" isRounded size={8} />
-         </Tooltip>
-      </div>
-   );
-}
+               <div className="text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               </div>
+            </div>
+         }
+      >
+         <Icon as="Info" color="blue" variant="shaded" isRounded size={8} />
+      </Tooltip>
+   </div>
+)
+
+export const TooltipWithText = () => (
+   <div className="my-10">
+      <Tooltip content="Now you know." color="indigo">
+         <Icon as="Info" color="indigo" variant="shaded" isRounded size={8} />
+      </Tooltip>
+   </div>
+);
--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Box from 'components/Box';
 
 const Tooltip = ({
+   color,
    content,
    children,
    className
@@ -11,13 +14,22 @@ const Tooltip = ({
    return (
       <div className={`tooltipwrapper relative inline-block ${className || ''}`} onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
          <div className={visible ? 'ml-2 absolute left-full top-1/2 transform -translate-y-1/2 text-sm w-80' : 'hidden'}>
-            <div className="inline-block p-4 bg-blue-0 rounded border-1 border-blue-200 shadow-md-blue animate-fade-in-to-r">
+            <Box color={color} variant="frame" className={`inline-block p-4 shadow-md-${color} animate-fade-in-to-r`}>
                {content}
-            </div>
+            </Box>
          </div>
          {children}
       </div>
    );
+}
+
+Tooltip.defaultProps = {
+   color: 'blue',
+}
+
+Tooltip.propTypes = {
+   color: PropTypes.string,
+   className: PropTypes.string,
 }
 
 export default Tooltip;
