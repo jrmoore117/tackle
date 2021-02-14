bf9ac9c04657f8052170fdc10bcd032f0a99e6bb
jrmoore117 <jrmoore117@gmail.com>
2021-02-13 20:54:39 -0800

    Repurpose Menu component name - now used for sidebar Menu component.

--- src/components/Menu/Menu.stories.js
+++ src/components/Menu/Menu.stories.js
@@ -1,104 +1,32 @@
 import React from 'react';
-import { Menu, MenuItem } from 'components/Menu';
-import Divider from 'components/Divider';
+import { Menu, MenuCategory, MenuItem } from 'components/Menu';
 
 export default {
    component: Menu,
-   title: 'Menu',
+   title: ' Menu',
 };
 
-export const MenuWithLabelAndIcons = () => {
-   const [isOpen, setIsOpen] = React.useState(false);
+export const BlueMenu = () => {
    return (
-      <Menu
-         isOpen={isOpen}
-         toggle={setIsOpen}
-         icon="MoreHorizontal"
-         className="mb-32"
-      >
-         <MenuItem icon="Share" label="Share" />
-         <MenuItem icon="Clipboard" label="Copy link" />
-         <MenuItem icon="Flag" label="Report" />
+      <Menu color="blue">
+         <MenuCategory label="Tackle" />
+         <MenuItem label="Hook" />
+         <MenuItem label="Line" isActive />
+         <MenuItem label="Sinker" />
+         <MenuCategory label="Numbers" />
+         <MenuItem label="One" />
+         <MenuItem label="Two" />
+         <MenuItem label="Three" />
       </Menu>
    );
 }
 
-export const BlueMenuWithLabel = () => {
-   const [isOpen, setIsOpen] = React.useState(false);
+export const BlueMenuWithIcons = () => {
    return (
-      <Menu
-         isOpen={isOpen}
-         toggle={setIsOpen}
-         color="blue"
-         label="Tackle"
-         className="mb-52"
-      >
-         <MenuItem label="Hook" />
-         <MenuItem label="Line" />
+      <Menu>
+         <MenuItem icon="User" label="Profile" />
+         <MenuItem icon="Bell" label="Notifications" isActive />
          <MenuItem label="Sinker" />
-         <Divider className="my-2" />
-         <MenuItem label="Bobber" />
-         <MenuItem label="Lure" />
-      </Menu>
-   );
-}
-
-export const BottomRightMenu = () => {
-   const [isOpen, setIsOpen] = React.useState(false);
-   return (
-      <Menu
-         isOpen={isOpen}
-         toggle={setIsOpen}
-         icon="MoreHorizontal"
-         position="br"
-         className="mb-52"
-      >
-         <MenuItem label="Hook" />
-         <MenuItem label="Line" />
-         <MenuItem label="Sinker" />
-         <Divider className="my-2" />
-         <MenuItem label="Bobber" />
-         <MenuItem label="Lure" />
-      </Menu>
-   );
-}
-
-export const TopRightMenu = () => {
-   const [isOpen, setIsOpen] = React.useState(false);
-   return (
-      <Menu
-         isOpen={isOpen}
-         toggle={setIsOpen}
-         position="tr"
-         className="mt-52"
-         icon="MoreHorizontal"
-      >
-         <MenuItem label="Hook" />
-         <MenuItem label="Line" />
-         <MenuItem label="Sinker" />
-         <Divider className="my-2" />
-         <MenuItem label="Bobber" />
-         <MenuItem label="Lure" />
-      </Menu>
-   );
-}
-
-export const TopLeftMenu = () => {
-   const [isOpen, setIsOpen] = React.useState(false);
-   return (
-      <Menu
-         isOpen={isOpen}
-         toggle={setIsOpen}
-         position="tl"
-         className="mt-52"
-         icon="MoreHorizontal"
-      >
-         <MenuItem label="Hook" />
-         <MenuItem label="Line" />
-         <MenuItem label="Sinker" />
-         <Divider className="my-2" />
-         <MenuItem label="Bobber" />
-         <MenuItem label="Lure" />
       </Menu>
    );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,88 +1,76 @@
-import React, { useRef, useEffect } from 'react';
+import React, { cloneElement } from 'react';
 import PropTypes from 'prop-types';
-import Button from 'components/Button';
 import Icon from 'components/Icon';
 
 export const Menu = ({
-   icon,
-   label,
    color,
-   toggle,
-   isOpen,
    variant,
-   position,
    children,
    className,
    ...props
-}) => {
-
-   const menuRef = useRef();
-
-   useEffect(() => {
-
-      const handleClickAway = (e) => {
-         if (menuRef.current.contains(e.target)) return;
-         toggle(false);
-      }
-
-      document.addEventListener("mousedown", handleClickAway);
-      
-      return () => {
-         document.removeEventListener("mousedown", handleClickAway);
-      };
-
-   }, [toggle]);
-
-   return (
-      <div
-         ref={menuRef}
-         onClick={() => toggle(!isOpen)}
-         className={`menuwrapper ${className || ''}`}
-      >
-         <Button
-            color={color}
-            variant={variant}
-            label={icon
-               ? <Icon as={icon} size={6} />
-               : label}
-            {...props}
-         />
-         <div className={isOpen ? `menu menu-to-${position}` : 'hidden'}>
-            {children}
-         </div>
-      </div>
-   );
-}
+}) => (
+   <div
+      className={`menu flex flex-col ${className || ''}`}
+      {...props}
+   >
+      {children.map((child, i) => cloneElement(child, {
+         key: `menuitem-${i}`,
+         color,
+      }))}
+   </div>
+);
 
 Menu.defaultProps = {
-   isOpen: false,
-   position: "bl",
+   color: 'blue'
 }
 
 Menu.propTypes = {
-   icon: PropTypes.string,
-   label: PropTypes.string,
    color: PropTypes.string,
    variant: PropTypes.string,
-   position: PropTypes.string,
    className: PropTypes.string,
-   isOpen: PropTypes.bool.isRequired,
-   toggle: PropTypes.func.isRequired,
 }
 
 export const MenuItem = ({
    icon,
    label,
+   color,
    onClick,
+   isActive,
    className,
    ...props
 }) => (
    <div
       onClick={onClick}
-      className={`menuitem ${className}`}
+      className={`menuitem px-2 py-1 mb-1 text-sm font-medium rounded flex items-center cursor-pointer hover:bg-gray-0 ${isActive ? `rounded bg-${color}-0 text-${color}-500` : ''} ${className}`}
       {...props}
    >
-      {icon && <Icon as={icon}  className="menuicon" />}
+      {icon && <Icon as={icon} color={isActive ? color : "gray"} variant="shaded" size={5} padding={1} className="menuicon" />}
+      {label}
+   </div>
+);
+
+MenuItem.propTypes = {
+   icon: PropTypes.string,
+   onClick: PropTypes.func,
+   className: PropTypes.string,
+   label: PropTypes.string.isRequired,
+}
+
+export const MenuCategory = ({
+   icon,
+   label,
+   color,
+   onClick,
+   isActive,
+   className,
+   ...props
+}) => (
+   <div
+      onClick={onClick}
+      className={`menuitem text-sm text-gray-700 font-medium pr-2 py-1 rounded ${isActive ? 'rounded bg-blue-0' : ''} ${className}`}
+      {...props}
+   >
+      {icon && <Icon as={icon} color={color} variant="shaded" className="menuicon" />}
       {label}
    </div>
 );
