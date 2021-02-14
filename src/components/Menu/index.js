ea9c300fda6522b662e3b70bdea9bda05231e317
jrmoore117 <jrmoore117@gmail.com>
2021-02-14 15:04:46 -0800

    Swap in Menu component classes. Add MenuItem and MenuCategory component ids - used to filter out invalid children in Menu component.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,24 +1,40 @@
-import React, { cloneElement } from 'react';
+import React, { isValidElement, cloneElement } from 'react';
 import PropTypes from 'prop-types';
-import Icon from 'components/Icon';
 
 export const Menu = ({
    color,
-   variant,
    children,
    className,
    ...props
-}) => (
-   <div
-      className={`menu flex flex-col ${className || ''}`}
-      {...props}
-   >
-      {children.map((child, i) => cloneElement(child, {
-         key: `menuitem-${i}`,
-         color,
-      }))}
-   </div>
-);
+}) => {
+   
+   const childrenWithProps = children.map((child, i) => {
+      
+      if (isValidElement(child) && child.type.id === 'MenuItem') {
+         return cloneElement(child, {
+            key: `menuitem-${i}`,
+            color,
+         });
+      }
+
+      if (isValidElement(child) && child.type.id === 'MenuCategory') {
+         return cloneElement(child, {
+            key: `menucategory-${i}`,
+         });
+      }
+
+      return null;
+   });
+   
+   return (
+      <div
+         className={`menu ${className || ''}`}
+         {...props}
+      >
+         {childrenWithProps}
+      </div>
+   );
+}
 
 Menu.defaultProps = {
    color: 'blue'
@@ -26,12 +42,10 @@ Menu.defaultProps = {
 
 Menu.propTypes = {
    color: PropTypes.string,
-   variant: PropTypes.string,
    className: PropTypes.string,
 }
 
 export const MenuItem = ({
-   icon,
    label,
    color,
    onClick,
@@ -41,42 +55,39 @@ export const MenuItem = ({
 }) => (
    <div
       onClick={onClick}
-      className={`menuitem px-2 py-1 mb-1 text-sm font-medium rounded flex items-center cursor-pointer hover:bg-gray-0 ${isActive ? `rounded bg-${color}-0 text-${color}-500` : ''} ${className}`}
+      className={`menuitem ${isActive ? `menuitem--active--${color}` : 'menuitem--inactive'} ${className || ''}`}
       {...props}
    >
-      {icon && <Icon as={icon} color={isActive ? color : "gray"} variant="shaded" size={5} padding={1} className="menuicon" />}
       {label}
    </div>
 );
 
+MenuItem.id = 'MenuItem';
+
 MenuItem.propTypes = {
-   icon: PropTypes.string,
    onClick: PropTypes.func,
    className: PropTypes.string,
    label: PropTypes.string.isRequired,
 }
 
 export const MenuCategory = ({
-   icon,
    label,
-   color,
    onClick,
-   isActive,
    className,
    ...props
 }) => (
    <div
       onClick={onClick}
-      className={`menuitem text-sm text-gray-700 font-medium pr-2 py-1 rounded ${isActive ? 'rounded bg-blue-0' : ''} ${className}`}
+      className={`menucategory ${className || ''}`}
       {...props}
    >
-      {icon && <Icon as={icon} color={color} variant="shaded" className="menuicon" />}
       {label}
    </div>
 );
 
+MenuCategory.id = 'MenuCategory';
+
 MenuItem.propTypes = {
-   icon: PropTypes.string,
    onClick: PropTypes.func,
    className: PropTypes.string,
    label: PropTypes.string.isRequired,
