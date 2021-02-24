04d329746a1a002004844f393beb9697f42de2a9
jrmoore117 <jrmoore117@gmail.com>
2021-02-23 22:40:38 -0800

    Add default values to initialValues and validators parameters in useForm hook.

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useForm = ({ initialValues, validators } = {}) => {
+const useForm = ({ initialValues = {}, validators = {} } = {}) => {
    const [values, setValues] = useState(initialValues);
    const [errors, setErrors] = useState({});
    return {
