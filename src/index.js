8e2f87a20ca5a45787bcb454e930c9348052157e
jrmoore117 <jrmoore117@gmail.com>
2021-03-10 22:46:02 -0800

    Import new stylesheet into app root.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import './tackle.output.css';
+import './css/tackle.output.css';
 
 ReactDOM.render(
   <App />,
