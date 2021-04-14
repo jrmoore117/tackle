6d2951ffca2d4aed08229e4f960fac071e047328
jrmoore117 <jrmoore117@gmail.com>
2021-04-13 19:35:32 -0700

    Add new index.js file to export all components/hooks for npm package.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './tackle.output.css';
-import './css/tackle.output.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+export * from 'components/Accordion';
+export * from 'components/ActionMenu';
+export * from 'components/Alert';
+export * from 'components/Avatar';
+export * from 'components/Badge';
+export * from 'components/Box';
+export * from 'components/Button';
+export * from 'components/Checkbox';
+export * from 'components/Chip';
+export * from 'components/Divider';
+export * from 'components/Drawer';
+export * from 'components/Field';
+export * from 'components/FieldElement';
+export * from 'components/FieldMessage';
+export * from 'components/Icon';
+export * from 'components/Menu';
+export * from 'components/Modal';
+export * from 'components/MultiSelect';
+export * from 'components/Radio';
+export * from 'components/Select';
+export * from 'components/Switch';
+export * from 'components/Tabs';
+export * from 'components/TextArea';
+export * from 'components/TextField';
+export * from 'components/Tooltip';
+export * from 'hooks/useForm';
