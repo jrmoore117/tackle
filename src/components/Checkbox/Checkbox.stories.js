2b390bb3376f2d9f42d8bfdb6aec5a44371c4ff3
jrmoore117 <jrmoore117@gmail.com>
2021-04-30 23:44:42 -0700

    Fix useForm import.

--- src/components/Checkbox/Checkbox.stories.js
+++ src/components/Checkbox/Checkbox.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Checkbox } from 'components/Checkbox';
-import useForm from 'hooks/useForm';
+import { useForm } from 'hooks/useForm';
 
 export default {
    component: Checkbox,
